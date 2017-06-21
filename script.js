f74ea0e023a0e5247a4e9548e710b3ec4bad01f6
Alyxe Lett <alyxe@scripted.org>
2017-06-21 15:41:10 +0000

    my cat site

--- script.js
+++ script.js
@@ -3,4 +3,24 @@
 $( document ).ready(function() {
 
 
+$("#signUp").click(function() {
+    var name = $("input").val();
+    $("#confirmation").append(name +  " you are confirmed to receive treats!");
+    
+});
+
+$("#answer").click(function() {
+    var response = $("#yesNo").val();
+    if (response=== "yes") {
+        alert("you rock!") {
+            else if(response === "no") {
+                alert("Wrong answer!")
+            } else {
+                alert("Choose again!")
+            }
+        }
+    }
+}
+
+
 });
